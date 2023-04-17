51cd0be8eca735257ac686ef97ce9d79b0eb1d08
jmcheon <developer.cheon@gmail.com>
2023-04-17 23:52:42 +0200

    feat: ex02 done

--- 01/ex02/test.py
+++ 01/ex02/test.py
@@ -39,12 +39,41 @@ if __name__ == "__main__":
 
 
 	# dot product
+	print("\n << dot product >>")
 	v1 = Vector([[2.0, 3.0, 1.0]])
 	v2 = Vector([[2.0], [3.0], [1.0]])
 	#v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
 	#v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+	print(v1)
 	print(v1.dot(v2))
+
+	# transpose
+	print("\n << transpose >>")
 	print(v1.T())
 	print(v1.T().shape)
 	print(v2.T())
 	print(v2.T().shape)
+
+	# division by scalar
+	v3 = v1 / 2.0
+	print(v3.values)
+	v3 = Vector(4) / 2
+	print(v3.values)
+	v3 = Vector(4) / 3.14
+	print(v3.values)
+	#v3 = Vector(4) / 0
+	#v3 = Vector(4) / None
+	#v3 = None / Vector(4) 
+	#v3 = 3 / Vector(4) 
+
+	# addition by scalar
+	v = Vector(4)
+	v2 = Vector([[1.0], [1.0], [1.0], [1.0]])
+	print((v + v2).values)
+	print((v - v2).values != (v2 - v).values)
+
+	# multiplication by scalar
+	v = Vector(4)
+	print(v * 4) 
+	print(4.0 * v) 
+	#v * "hello"
--- 01/ex02/vector.py
+++ 01/ex02/vector.py
@@ -1,8 +1,8 @@
 
 class Vector:
 	"""
-	• values: list of list of floats (for row vector) or list of lists of single float (for column vector),
-	• shape: tuple of 2 integers: (1,n) for a row vector of dimension n or (n,1) for a 	column vector of dimension n.
+		• values: list of list of floats (for row vector) or list of lists of single float (for column vector).
+		• shape: tuple of 2 integers: (1,n) for a row vector of dimension n or (n,1) for a column vector of dimension n.
 	"""
 	def __init__(self, data):
 		self.values = []
@@ -12,12 +12,10 @@ class Vector:
 			if len(data) == 1 and isinstance(data[0], list) and len(data[0]) > 0 and all(isinstance(i, float) for i in data[0]):	
 				self.values = data
 				self.shape = (1, len(data[0]))
-				print(self.values, self.shape)
 			# initialize a list of lists of single float : Vector([[0.0], [1.0], [2.0], [3.0]])
 			elif all(isinstance(elem, list) and len(elem) == 1 and all(isinstance(i, float) for i in elem) for elem in data):
 				self.values = data
 				self.shape = (len(data), 1)
-				print(self.values, self.shape)
 			else:
 				raise ValueError("Invalid form of list,", data)
 
@@ -27,7 +25,6 @@ class Vector:
 				raise ValueError("Size must be a positive integer")
 			self.values = [[float(i)] for i in range(data)]
 			self.shape = (data, 1)
-			#print(self.values, self.shape)
 
 		# when data is a tuple
 		elif isinstance(data, tuple) and len(data) == 2 and all(isinstance(elem, int) and elem >= 0 for elem in data):
@@ -35,7 +32,6 @@ class Vector:
 				raise ValueError("First value of tuple must be greater than the second one")
 			self.values = [[float(i)] for i in range(data[0], data[1])]
 			self.shape = (data[1] - data[0], 1)
-			#print(self.values, self.shape)
 		else:
 			raise ValueError("Invalid form of data,", data)
 
@@ -59,11 +55,82 @@ class Vector:
 			transposed.append(row)
 		return Vector(transposed)
 
+	# add & radd : only vectors of same shape.
+	def __add__(self, other):
+		if not isinstance(other, Vector):
+			raise TypeError("Invalid input: dot product requires a Vector object.")
+		if self.shape != other.shape:
+			raise TypeError("Invalid input: dot product requires a Vector of same shape.")
+		result = []
+		for i in range(self.shape[0]):
+			row = []
+			for j in range(self.shape[1]):
+				row.append(self.values[i][j] + other.values[i][j])
+			result.append(row)
+		return Vector(result)
+	
+	def __radd__(self, other):
+		return other + self
 
+	# sub & rsub: only vectors of same shape.
+	def __sub__(self, other):
+		if not isinstance(other, Vector):
+			raise TypeError("Invalid input: dot product requires a Vector object.")
+		if self.shape != other.shape:
+			raise TypeError("Invalid input: dot product requires a Vector of same shape.")
+		result = []
+		for i in range(self.shape[0]):
+			row = []
+			for j in range(self.shape[1]):
+				row.append(self.values[i][j] - other.values[i][j])
+			result.append(row)
+		return Vector(result)
+	
+	def __rsub__(self, other):
+		return other - self
 
-# add & radd : only vectors of same shape.
-# sub & rsub: only vectors of same shape.
-# truediv : only with scalars (to perform division of Vector by a scalar).
-# rtruediv : raises an NotImplementedError with the message "Division of a scalar by a Vector is not defined here."
-# mul & rmul: only scalars (to perform multiplication of Vector by a scalar).
-# must be identical, i.e we expect that print(vector) and vector within python interpretor behave the same, see correspond
+	# truediv : only with scalars (to perform division of Vector by a scalar).
+	def __truediv__(self, scalar):
+		if isinstance(scalar, Vector):
+			raise NotImplementedError("Division with a Vector object is not implemented.")
+		if not any(isinstance(scalar, scalar_type) for scalar_type in [int, float, complex]):
+			raise TypeError("Invalid type of scalar value.")
+		if scalar == 0:
+			raise ValueError("Can't not divide by 0.")
+		result = []
+		for i in range(self.shape[0]):
+			row = []
+			for j in range(self.shape[1]):
+				row.append(self.values[i][j] / scalar)
+			result.append(row)
+		return Vector(result)
+
+	# rtruediv : raises an NotImplementedError with the message "Division of a scalar by a Vector is not defined here."
+	def __rtruediv__(self, scalar):
+		raise NotImplementedError("Division of a scalar by a Vector is not defined here.")
+
+	# mul & rmul: only scalars (to perform multiplication of Vector by a scalar).
+	def __mul__(self, scalar):
+		if isinstance(scalar, Vector):
+			raise NotImplementedError("Multiplication with a Vector object is not implemented.")
+		if not any(isinstance(scalar, scalar_type) for scalar_type in [int, float, complex]):
+			raise TypeError("Invalid type of scalar value.")
+		result = []
+		for i in range(self.shape[0]):
+			row = []
+			for j in range(self.shape[1]):
+				row.append(self.values[i][j] * scalar)
+			result.append(row)
+		return Vector(result)
+
+	def __rmul__(self, scalar):
+		return self * scalar
+
+	# must be identical, i.e we expect that print(vector) and vector within python interpretor behave the same, see correspond
+	def __str__(self):
+		txt = "{} {}".format(self.values, self.shape)
+		return txt
+
+	def __repr__(self):
+		txt = "{} {}".format(self.values, self.shape)
+		return txt
