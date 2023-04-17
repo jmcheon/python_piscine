c2b6828e141c203ca221e4c815a7d9c7bdcecdb2
jmcheon <developer.cheon@gmail.com>
2023-04-17 11:19:51 +0200

    feat: python module 01 directory

---
+++ 01/ex00/recipe.py
@@ -0,0 +1,19 @@
+
+
+class Recipe:
+"""
+  name (str): name of the recipe
+• cooking_lvl (int): range from 1 to 5,
+• cooking_time (int): in minutes (no negative numbers),
+• ingredients (list): list of all ingredients each represented by a string,
+• description (str): description of the recipe,
+• recipe_type (str): can be "starter", "lunch" or "dessert"
+"""
+
+	def __init__(self):
+		self.name = name
+		self.cooking_lvl = cooking_lvl
+		self.cooking_time = cooking_time
+		self.ingredients = ingredients
+		self.description = description
+		self.recipe_type = recipe_type
