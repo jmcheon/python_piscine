f2688df118fd801cb48cbe3add0f887e60738186
jmcheon <developer.cheon@gmail.com>
2023-05-01 02:14:57 +0200

    feat: new test.py for scatter plots

--- 03/ex04/Kmeans.py
+++ 03/ex04/Kmeans.py
@@ -1,4 +1,6 @@
 import sys
+import numpy as np
+from csvreader import CsvReader
 
 class KmeansClustering:
 
@@ -21,6 +23,33 @@ class KmeansClustering:
 		-------
 		This function should not raise any Exception.
 		"""
+		# randomly select n centroids from the dataset
+		selected_numbers = np.random.choice(X.shape[0], self.ncentroid, replace=False)
+		self.centroids = X[selected_numbers, :]
+		print("X.shape:", X.shape)
+		print("array of randomly selected numbers:", selected_numbers)
+		print(self.centroids)
+		print("self.centroids.shape:", self.centroids.shape)
+
+		print("X.shape:", X.shape)
+		print("self.centroids[:, np.newaxis, :].shape:", self.centroids[:, np.newaxis, :].shape)
+		distances = X - self.centroids[:, np.newaxis, :]
+		print(distances)
+		# loop through the max iterations to update the centroids
+	#	for i in range(self.max_iter):
+			# calculate the Euclidean distance between each point and centroid
+			#distances = np.sqrt(np.sum((X - self.centroids[:, np.newaxis, :])**2, axis=2))
+			#clusters = np.argmin(distances, axis=0)
+#		for i in range(self.max_iter):
+#			# Calculate the Euclidean distance between each point and centroid
+#			distances = np.sqrt(np.sum((X - self.centroids[:, np.newaxis, :])**2, axis=2))
+#			# Assign each point to the closest centroid
+#			clusters = np.argmin(distances, axis=0)
+#			# Update the centroids to the mean of each cluster
+#			for j in range(self.ncentroid):
+#				self.centroids[j] = np.mean(X[clusters==j], axis=0)
+			
+
 		
 	def predict(self, X):
 		"""
@@ -39,5 +68,14 @@ class KmeansClustering:
 
 if __name__ == "__main__":
 	if len(sys.argv) != 4:
+		#python Kmeans.py filepath='<path_to_solar_system_census_csv_file>' ncentroid=4 max_iter=30
 		print("Usage: python Kmean.py [filepath] [max_iter] [ncentroid]")
 	else:
+		k = KmeansClustering()
+		filename = sys.argv[1]
+		with CsvReader(filename, header=True) as reader:
+			if reader == None:
+				print("File is corrupted or missing")
+			else:
+				X = np.array(reader.getdata())
+				k.fit(X.astype(float))
---
+++ 03/ex04/test.py
@@ -0,0 +1,73 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from csvreader import CsvReader
+from mpl_toolkits import mplot3d
+
+
+def plot_3by2():
+	fig, axs = plt.subplots(3, 2)
+	# Create scatter images
+	axs[0, 0].scatter(height, weight)
+	axs[0, 0].set_xlabel("height")
+	axs[0, 0].set_ylabel("weight")
+
+	axs[0, 1].scatter(height, bone_density)
+	axs[0, 1].set_xlabel("height")
+	axs[0, 1].set_ylabel("bone_density")
+
+	axs[1, 0].scatter(weight, height)
+	axs[1, 0].set_xlabel("weight")
+	axs[1, 0].set_ylabel("height")
+
+	axs[1, 1].scatter(weight, bone_density)
+	axs[1, 1].set_xlabel("weight")
+	axs[1, 1].set_ylabel("bone density")
+
+	axs[2, 0].scatter(bone_density, height)
+	axs[2, 0].set_xlabel("bone density")
+	axs[2, 0].set_ylabel("height")
+	
+	axs[2, 1].scatter(bone_density, height)
+	axs[2, 1].set_xlabel("bone density")
+	axs[2, 1].set_ylabel("height")
+
+	plt.show()
+
+def plot_3d():
+	# Create a figure and a 3D subplot
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	
+	# Create a scatter plot
+	ax.scatter(height, weight, bone_density)
+	
+	# Set labels for the axes
+	ax.set_xlabel('height')
+	ax.set_ylabel('weight')
+	ax.set_zlabel('bone density')
+	
+	# Show the plot
+	plt.show()
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print(f"Usage: python {sys.argv[0]} [filepath]")
+		exit()
+
+	filename = sys.argv[1]
+	with CsvReader(filename, header=True) as reader:
+		if reader == None:
+			print("File is corrupted or missing")
+			exit()
+		else:
+			X = reader.getdata()
+			header = reader.getheader()
+	
+	# extract columns 'height' and 'weight' and 'bone_density' from the dataframe
+	height = [float(row[1]) for row in X]
+	weight = [float(row[2]) for row in X]
+	bone_density = [float(row[3]) for row in X]
+
+	plot_3by2()
+	plot_3d()
