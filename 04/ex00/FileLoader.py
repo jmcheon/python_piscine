e0b6e11224b602daca306174bffb3232dc3c44b0
jmcheon <developer.cheon@gmail.com>
2023-05-02 14:30:25 +0200

    fix: invalid input error

--- 04/ex00/FileLoader.py
+++ 04/ex00/FileLoader.py
@@ -24,10 +24,13 @@ class FileLoader:
 		displays the first n rows of the dataset if n is positive, or the last n rows if n is
 		negative.
 		"""
-		if n > 0:
-			print(df.head(n))
+		if isinstance(n, int):
+			if n > 0:
+				print(df.head(n))
+			else:
+				print(df.tail(abs(n)))
 		else:
-			print(df.tail(abs(n)))
+			print("Invalid input: n should be an integer.")
 
 if __name__ == "__main__":
 	loader = FileLoader()
--- 04/ex02/FileLoader.py
+++ 04/ex02/FileLoader.py
@@ -24,13 +24,16 @@ class FileLoader:
 		displays the first n rows of the dataset if n is positive, or the last n rows if n is
 		negative.
 		"""
-		if n > 0:
-			print(df.head(n))
+		if isinstance(n, int):
+			if n > 0:
+				print(df.head(n))
+			else:
+				print(df.tail(abs(n)))
 		else:
-			print(df.tail(abs(n)))
+			print("Invalid input: n should be an integer.")
 
 if __name__ == "__main__":
 	loader = FileLoader()
-	data = loader.load("./athlete_events.csv")
+	data = loader.load("../athlete_events.csv")
 	loader.display(data, 12)
 	#loader.display(data, -42)
